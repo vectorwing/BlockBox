b1ee3c5be0daf456f210f53a584233850e6db07c
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2025-01-14 18:13:32 -0300

    Add client packet to update mallet block motion

--- src/main/java/vectorwing/buildingbigger/common/item/MalletItem.java
+++ src/main/java/vectorwing/buildingbigger/common/item/MalletItem.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
@@ -16,12 +18,12 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.piston.MovingPistonBlock;
-import net.minecraft.world.level.block.piston.PistonHeadBlock;
 import net.minecraft.world.level.block.piston.PistonStructureResolver;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.PistonType;
-import net.minecraft.world.level.gameevent.GameEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
+import vectorwing.buildingbigger.BuildingBigger;
+import vectorwing.buildingbigger.common.network.payload.MalletPushPayload;
 
 import java.util.List;
 import java.util.Map;
@@ -39,112 +41,105 @@ public class MalletItem extends Item
 		Level level = context.getLevel();
 		BlockPos clickedPos = context.getClickedPos();
 		Direction clickedFace = context.getClickedFace();
+		Direction pushDirection = clickedFace.getOpposite();
 		BlockPos pos = clickedPos.relative(clickedFace);
+		Player player = context.getPlayer();
 
-		if (this.moveBlocks(level, pos, context.getClickedFace().getOpposite())) {
-			level.playSound(null, pos, SoundEvents.ZOMBIE_ATTACK_IRON_DOOR, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.25F + 0.6F);
-			Player player = context.getPlayer();
-			if (player != null) {
-				player.getCooldowns().addCooldown(this, 20);
+		if (level.isClientSide) {
+			PistonStructureResolver structureResolver = new PistonStructureResolver(level, pos, pushDirection, true);
+			if (structureResolver.resolve()) {
+				return InteractionResult.SUCCESS;
 			}
-			return InteractionResult.sidedSuccess(level.isClientSide);
+			return InteractionResult.PASS;
+		}
+
+		if (moveBlocks(level, pos, context.getClickedFace().getOpposite())) {
+			level.playSound(null, pos, SoundEvents.ZOMBIE_ATTACK_IRON_DOOR, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.25F + 0.6F);
+			if (player != null) {
+				player.getCooldowns().addCooldown(this, 10);
+			}
+			PacketDistributor.sendToPlayersTrackingChunk((ServerLevel) level, level.getChunkAt(pos).getPos(), new MalletPushPayload(pos, pushDirection));
+			return InteractionResult.CONSUME;
 		} else {
 			level.playSound(null, pos, SoundEvents.NETHERITE_BLOCK_HIT, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.25F + 0.6F);
+			if (player != null) {
+				player.displayClientMessage(Component.translatable(BuildingBigger.MODID + ".item.mallet.failure"), true);
+			}
 			return InteractionResult.PASS;
 		}
 	}
 
-	protected boolean moveBlocks(Level level, BlockPos pos, Direction facing) {
-		BlockPos blockpos = pos.relative(facing);
-		boolean extending = true;
-
-		PistonStructureResolver structureResolver = new PistonStructureResolver(level, pos, facing, extending);
+	public static boolean moveBlocks(Level level, BlockPos pos, Direction facing) {
+		PistonStructureResolver structureResolver = new PistonStructureResolver(level, pos, facing, true);
 		if (!structureResolver.resolve()) {
 			return false;
-		} else {
-			Map<BlockPos, BlockState> map = Maps.newHashMap();
-			List<BlockPos> list = structureResolver.getToPush();
-			List<BlockState> list1 = Lists.newArrayList();
-
-			for (BlockPos blockpos1 : list) {
-				BlockState blockstate = level.getBlockState(blockpos1);
-				list1.add(blockstate);
-				map.put(blockpos1, blockstate);
-			}
-
-			List<BlockPos> list2 = structureResolver.getToDestroy();
-			BlockState[] ablockstate = new BlockState[list.size() + list2.size()];
-			int i = 0;
-
-			for (int j = list2.size() - 1; j >= 0; j--) {
-				BlockPos blockpos2 = list2.get(j);
-				BlockState blockstate1 = level.getBlockState(blockpos2);
-				BlockEntity blockentity = blockstate1.hasBlockEntity() ? level.getBlockEntity(blockpos2) : null;
-				dropResources(blockstate1, level, blockpos2, blockentity);
-				blockstate1.onDestroyedByPushReaction(level, blockpos2, facing, level.getFluidState(blockpos2));
-				if (!blockstate1.is(BlockTags.FIRE)) {
-					level.addDestroyBlockEffect(blockpos2, blockstate1);
-				}
-
-				ablockstate[i++] = blockstate1;
-			}
-
-			for (int k = list.size() - 1; k >= 0; k--) {
-				BlockPos blockpos3 = list.get(k);
-				BlockState blockstate5 = level.getBlockState(blockpos3);
-				blockpos3 = blockpos3.relative(facing);
-				map.remove(blockpos3);
-				BlockState blockstate8 = Blocks.MOVING_PISTON.defaultBlockState().setValue(DirectionalBlock.FACING, facing);
-				level.setBlock(blockpos3, blockstate8, 68);
-				level.setBlockEntity(MovingPistonBlock.newMovingBlockEntity(blockpos3, blockstate8, list1.get(k), facing, extending, false));
-				ablockstate[i++] = blockstate5;
-			}
-
-
-//			PistonType pistontype = PistonType.DEFAULT;
-//			BlockState blockstate4 = Blocks.PISTON_HEAD
-//					.defaultBlockState()
-//					.setValue(PistonHeadBlock.FACING, facing)
-//					.setValue(PistonHeadBlock.TYPE, pistontype);
-//			BlockState blockstate6 = Blocks.MOVING_PISTON
-//					.defaultBlockState()
-//					.setValue(MovingPistonBlock.FACING, facing)
-//					.setValue(MovingPistonBlock.TYPE, PistonType.DEFAULT);
-//			map.remove(blockpos);
-//			level.setBlock(blockpos, blockstate6, 68);
-//			level.setBlockEntity(MovingPistonBlock.newMovingBlockEntity(blockpos, blockstate6, blockstate4, facing, true, true));
-
-
-			BlockState blockstate3 = Blocks.AIR.defaultBlockState();
-
-			for (BlockPos blockpos4 : map.keySet()) {
-				level.setBlock(blockpos4, blockstate3, 82);
-			}
-
-			for (Map.Entry<BlockPos, BlockState> entry : map.entrySet()) {
-				BlockPos blockpos5 = entry.getKey();
-				BlockState blockstate2 = entry.getValue();
-				blockstate2.updateIndirectNeighbourShapes(level, blockpos5, 2);
-				blockstate3.updateNeighbourShapes(level, blockpos5, 2);
-				blockstate3.updateIndirectNeighbourShapes(level, blockpos5, 2);
-			}
-
-			i = 0;
-
-			for (int l = list2.size() - 1; l >= 0; l--) {
-				BlockState blockstate7 = ablockstate[i++];
-				BlockPos blockpos6 = list2.get(l);
-				blockstate7.updateIndirectNeighbourShapes(level, blockpos6, 2);
-				level.updateNeighborsAt(blockpos6, blockstate7.getBlock());
-			}
-
-			for (int i1 = list.size() - 1; i1 >= 0; i1--) {
-				level.updateNeighborsAt(list.get(i1), ablockstate[i++].getBlock());
-			}
-
-			level.updateNeighborsAt(blockpos, Blocks.PISTON_HEAD);
-
-			return true;
 		}
+
+		Map<BlockPos, BlockState> blocksToPush = Maps.newHashMap();
+		List<BlockPos> positionsToPush = structureResolver.getToPush();
+		List<BlockState> statesToPush = Lists.newArrayList();
+
+		for (BlockPos posToPush : positionsToPush) {
+			BlockState stateToPush = level.getBlockState(posToPush);
+			statesToPush.add(stateToPush);
+			blocksToPush.put(posToPush, stateToPush);
+		}
+
+		List<BlockPos> positionsToDestroy = structureResolver.getToDestroy();
+		BlockState[] positionsToAffect = new BlockState[positionsToPush.size() + positionsToDestroy.size()];
+		int i = 0;
+
+		for (int j = positionsToDestroy.size() - 1; j >= 0; j--) {
+			BlockPos destroyedPos = positionsToDestroy.get(j);
+			BlockState destroyedState = level.getBlockState(destroyedPos);
+			BlockEntity destroyedBlockEntity = destroyedState.hasBlockEntity() ? level.getBlockEntity(destroyedPos) : null;
+			dropResources(destroyedState, level, destroyedPos, destroyedBlockEntity);
+			destroyedState.onDestroyedByPushReaction(level, destroyedPos, facing, level.getFluidState(destroyedPos));
+			if (!destroyedState.is(BlockTags.FIRE)) {
+				level.addDestroyBlockEffect(destroyedPos, destroyedState);
+			}
+
+			positionsToAffect[i++] = destroyedState;
+		}
+
+		for (int k = positionsToPush.size() - 1; k >= 0; k--) {
+			BlockPos pushedPos = positionsToPush.get(k);
+			BlockState pushedState = level.getBlockState(pushedPos);
+			pushedPos = pushedPos.relative(facing);
+			blocksToPush.remove(pushedPos);
+			BlockState movingBlockState = Blocks.MOVING_PISTON.defaultBlockState().setValue(DirectionalBlock.FACING, facing);
+			level.setBlock(pushedPos, movingBlockState, 68);
+			level.setBlockEntity(MovingPistonBlock.newMovingBlockEntity(pushedPos, movingBlockState, statesToPush.get(k), facing, true, false));
+			positionsToAffect[i++] = pushedState;
+		}
+
+		BlockState airState = Blocks.AIR.defaultBlockState();
+
+		for (BlockPos finishedPos : blocksToPush.keySet()) {
+			level.setBlock(finishedPos, airState, 82);
+		}
+
+		for (Map.Entry<BlockPos, BlockState> entry : blocksToPush.entrySet()) {
+			BlockPos blockpos5 = entry.getKey();
+			BlockState blockstate2 = entry.getValue();
+			blockstate2.updateIndirectNeighbourShapes(level, blockpos5, 2);
+			airState.updateNeighbourShapes(level, blockpos5, 2);
+			airState.updateIndirectNeighbourShapes(level, blockpos5, 2);
+		}
+
+		i = 0;
+
+		for (int l = positionsToDestroy.size() - 1; l >= 0; l--) {
+			BlockState blockstate7 = positionsToAffect[i++];
+			BlockPos blockpos6 = positionsToDestroy.get(l);
+			blockstate7.updateIndirectNeighbourShapes(level, blockpos6, 2);
+			level.updateNeighborsAt(blockpos6, blockstate7.getBlock());
+		}
+
+		for (int i1 = positionsToPush.size() - 1; i1 >= 0; i1--) {
+			level.updateNeighborsAt(positionsToPush.get(i1), positionsToAffect[i++].getBlock());
+		}
+
+		return true;
 	}
 }
