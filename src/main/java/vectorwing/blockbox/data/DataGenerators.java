819024adcbd44ae0b72ac655f38c036b95af839c
Thomas Kain <kain.tommy@gmail.com>
2026-04-02 10:29:10 -0400

    26.1 port, part 5: Fix up data generation

--- src/main/java/vectorwing/blockbox/data/DataGenerators.java
+++ src/main/java/vectorwing/blockbox/data/DataGenerators.java
@@ -1,13 +1,7 @@
 package vectorwing.blockbox.data;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import vectorwing.blockbox.BlockBox;
 import vectorwing.blockbox.data.provider.*;
@@ -15,37 +9,24 @@ import vectorwing.blockbox.data.tag.BlockTags;
 import vectorwing.blockbox.data.tag.DamageTypeTags;
 import vectorwing.blockbox.data.tag.ItemTags;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 @SuppressWarnings("unused")
-@EventBusSubscriber(modid = BlockBox.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = BlockBox.MODID)
 public class DataGenerators
 {
 	@SubscribeEvent
-	public static void gatherData(GatherDataEvent event) {
-		DataGenerator generator = event.getGenerator();
-		PackOutput output = generator.getPackOutput();
-		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-		ExistingFileHelper helper = event.getExistingFileHelper();
-
+	public static void gatherData(GatherDataEvent.Client event) {
 		// Data packs
-		BlockTags blockTags = new BlockTags(output, lookupProvider, helper);
-		generator.addProvider(event.includeServer(), blockTags);
-		generator.addProvider(event.includeServer(), new ItemTags(output, lookupProvider, blockTags.contentsGetter(), helper));
-		generator.addProvider(event.includeServer(), new DamageTypeTags(output, lookupProvider, helper));
+		event.createDatapackRegistryObjects(BuiltInEntries.BUILDER);
 
-		generator.addProvider(event.includeServer(), new Recipes(output, lookupProvider));
-		generator.addProvider(event.includeServer(), new DataMaps(output, lookupProvider));
-		generator.addProvider(event.includeServer(), new LootTableProvider(output, Collections.emptySet(), List.of(
-				new LootTableProvider.SubProviderEntry(BlockLootTables::new, LootContextParamSets.BLOCK)
-		), lookupProvider));
+		event.createBlockAndItemTags(BlockTags::new, ItemTags::new);
+		event.createProvider(DamageTypeTags::new);
+
+		event.createProvider(Recipes.Runner::new);
+		event.createProvider(DataMaps::new);
+		event.createProvider(LootTables::new);
 
 		// Resource packs
-		BlockStates blockStates = new BlockStates(output, helper);
-		generator.addProvider(event.includeClient(), blockStates);
-		generator.addProvider(event.includeClient(), new ItemModels(output, blockStates.models().existingFileHelper));
-		generator.addProvider(event.includeClient(), new ParticleTypes(output, helper));
+		event.createProvider(BlockItemModels::new);
+		event.createProvider(ParticleTypes::new);
 	}
 }
---
+++ src/main/java/vectorwing/blockbox/data/provider/LootTables.java
@@ -0,0 +1,19 @@
+package vectorwing.blockbox.data.provider;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class LootTables extends LootTableProvider
+{
+	public LootTables(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+		super(output, Collections.emptySet(), List.of(
+				new LootTableProvider.SubProviderEntry(BlockLootTables::new, LootContextParamSets.BLOCK)
+		), registries);
+	}
+}
